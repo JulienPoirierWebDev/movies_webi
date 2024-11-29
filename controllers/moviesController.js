0fd53eff61026c9b3c8377fd089af778437bd86a
Julien POIRIER <connexion@julienpoirier-Webdev.com>
2024-11-29 15:02:19 +0100

    refacto moviesModel with mongoose

--- controllers/moviesController.js
+++ controllers/moviesController.js
@@ -11,22 +11,13 @@ const getAllMovies = async (req, res) => {
 
 const getOneById = async (req, res) => {
 	const targetId = Number(req.params.id);
-
-	if (isNaN(targetId)) {
+	if (!mongoose.isValidObjectId(targetId)) {
 		res.json({
 			error: true,
 			message: "Cet id n'est pas valide",
 		});
 	} else {
-		const data = await MoviesModel.getOneById();
-
-		const cleanData = JSON.parse(data);
-
-		const movie = cleanData.filter((oneMovie) => {
-			if (oneMovie.id === targetId) {
-				return oneMovie;
-			}
-		})[0];
+		const movie = await MoviesModel.getOneById(targetId);
 
 		if (!movie) {
 			res.status(404).json({
@@ -43,86 +34,35 @@ const createOne = async (req, res) => {
 	try {
 		const newMovie = req.body;
 
-		/*
-		"adult": false,
-		"backdrop_path": null,
-		"genre_ids": [27],
-		"id": 1268729,
-		"original_language": "ja",
-		"original_title": "投稿 怨霊映像 Vol.106 変篇",
-		"overview": "This is the 106th installment of the “Posted Grudge Spirit Footage” film series.",
-		"popularity": 0.087,
-		"poster_path": "/mAg64xSP5ZfQMgMf9rtbDhikydh.jpg",
-		"release_date": "2024-03-06",
-		"title": "Posted Grudge Spirit Footage Vol.106: Transformation Edition",
-		"video": false,
-		"vote_average": 0,
-		"vote_count": 0
-		 */
-
-		// check if all required fields are present
-
-		if (
-			!newMovie.adult ||
-			!newMovie.backdrop_path ||
-			!newMovie.genre_ids ||
-			!newMovie.id ||
-			!newMovie.original_language ||
-			!newMovie.original_title ||
-			!newMovie.overview ||
-			!newMovie.popularity ||
-			!newMovie.poster_path ||
-			!newMovie.release_date ||
-			!newMovie.title ||
-			!newMovie.video ||
-			!newMovie.vote_average ||
-			!newMovie.vote_count
-		) {
-			res.status(400).json({
-				error: true,
-				message: 'Tous les champs sont obligatoires',
-			});
-			return;
-		}
-
-		// check format of required fields
-
-		if (
-			typeof newMovie.adult !== 'boolean' ||
-			typeof newMovie.backdrop_path !== 'string' ||
-			!Array.isArray(newMovie.genre_ids) ||
-			typeof newMovie.id !== 'number' ||
-			typeof newMovie.original_language !== 'string' ||
-			typeof newMovie.original_title !== 'string' ||
-			typeof newMovie.overview !== 'string' ||
-			typeof newMovie.popularity !== 'number' ||
-			typeof newMovie.poster_path !== 'string' ||
-			typeof newMovie.release_date !== 'string' ||
-			typeof newMovie.title !== 'string' ||
-			typeof newMovie.video !== 'boolean' ||
-			typeof newMovie.vote_average !== 'number' ||
-			typeof newMovie.vote_count !== 'number'
-		) {
-			res.status(400).json({
-				error: true,
-				message: 'Mauvais format',
-			});
-			return;
-		}
-
 		const data = await MoviesModel.createOne(newMovie);
-		res.status(201).json({ message: 'Film ajouté' });
+		res.status(201).json({ message: 'Film ajouté', data });
 	} catch (error) {
 		res.status(500).json({ error: true, message: error.message });
 	}
 };
 
-const patchOneById = (req, res) => {
-	res.json({ message: 'Je suis une route patch de movies' });
+const patchOneById = async (req, res) => {
+	try {
+		const update = req.body.update;
+		const _id = req.body._id;
+
+		const data = await MoviesModel.updateOneById(update, _id);
+
+		res.status(200).json({ message: 'Modification réussie', data });
+	} catch (error) {
+		res.status(500).json({ error: true, message: error.message });
+	}
 };
 
-const deleteOneById = (req, res) => {
-	res.json({ message: 'Je suis une route delete de movies' });
+const deleteOneById = async (req, res) => {
+	try {
+		const _id = req.body._id;
+		const data = await MoviesModel.deleteOneById(_id);
+
+		res.status(200).json({ message: 'Suppression réussie', data });
+	} catch (error) {
+		res.status(500).json({ error: true, message: error.message });
+	}
 };
 
 export { getAllMovies, getOneById, createOne, patchOneById, deleteOneById };
--- models/moviesModel.js
+++ models/moviesModel.js
@@ -1,16 +1,43 @@
-import fs from 'node:fs/promises';
+import mongoose, { Schema } from 'mongoose';
+
+const MovieSchema = Schema({
+	adult: Boolean,
+	backdrop_path: String,
+	genre_ids: [Number],
+	id: Number,
+	original_language: String,
+	original_title: {
+		type: String,
+		required: true,
+	},
+	overview: String,
+	popularity: Number,
+	poster_path: String,
+	release_date: String,
+	title: String,
+	video: Boolean,
+	vote_average: Number,
+	vote_count: Number,
+});
+
+const Movies = mongoose.model('Movies', MovieSchema);
 
 class MoviesModel {
 	static async getAllMovies() {
 		try {
-			const data = await fs.readFile('movies.json', { encoding: 'utf8' });
+			/*const data = await fs.readFile('movies.json', { encoding: 'utf8' });
 			const cleanData = JSON.parse(data);
-			return cleanData;
+			return cleanData;*/
+			const data = await Movies.find();
+			return data;
 		} catch (error) {
 			console.log(error);
 		}
 	}
 	static async getOneById(id) {
+		const movie = await Movies.findById(id);
+		return movie;
+		/*
 		const data = await fs.readFile('movies.json', { encoding: 'utf8' });
 		const cleanData = JSON.parse(data);
 		const movie = cleanData.filter((oneMovie) => {
@@ -19,8 +46,26 @@ class MoviesModel {
 			}
 		})[0];
 		return movie;
+		*/
 	}
 	static async createOne(newMovie) {
+		try {
+			const isMovieWithSameIdTMDB = await Movies.findOne({
+				id: newMovie.id,
+			});
+
+			if (isMovieWithSameIdTMDB) {
+				return 'Ce film existe déjà';
+			} else {
+				const movieIsMongoose = new Movies(newMovie);
+				movieIsMongoose.save();
+				return 'Film ajouté avec succès';
+			}
+		} catch (error) {
+			console.error(error);
+		}
+
+		/*
 		const allMovies = await MoviesModel.getAllMovies();
 
 		const alreadyExist = allMovies.some(
@@ -34,14 +79,31 @@ class MoviesModel {
 			allMovies.push({ ...newMovie });
 
 			try {
-				const data = await fs.writeFile('movies.json', JSON.stringify(allMovies));
+				const data = await fs.writeFile(
+					'movies.json',
+					JSON.stringify(allMovies)
+				);
 				return 'Film ajouté avec succès';
 			} catch (error) {
 				throw new Error(
 					"Erreur lors de l'écriture du fichier : " + error.message
 				);
 			}
+		}*/
+	}
+	static async updateOneById(update, _id) {
+		try {
+			const movieUpdated = await Movies.findByIdAndUpdate(_id, update);
+			return movieUpdated;
+		} catch (error) {
+			console.log(error);
 		}
+	}
+	static async deleteOneById(_id) {
+		try {
+			const deletedMovie = await Movies.findByIdAndDelete(_id);
+			return deletedMovie;
+		} catch (error) {}
 	}
 }
 
