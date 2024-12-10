59933f141dcc2eea73cc327acd4f6170a2061ea1
Julien POIRIER <connexion@julienpoirier-Webdev.com>
2024-12-10 09:16:28 +0100

    db

--- services/db.js
+++ services/db.js
@@ -3,7 +3,7 @@ import mongoose from 'mongoose';
 async function connectDB() {
 	try {
 		await mongoose.connect(
-			`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.rylpson.mongodb.net/webitech?retryWrites=true&w=majority&appName=teachingCluster`
+			`mongodb+srv://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_CLUSTER}.mongodb.net/webitech?retryWrites=true&w=majority&appName=teachingCluster`
 		);
 		console.log('Connexion à la base de données réussie !');
 	} catch (error) {
